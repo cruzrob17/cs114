c7cbf5d96b1db7d9bdc49ac352c1deae727cdc9e
cruzrob17 <rcruz2827@gmail.com>
2017-09-04 17:36:50 -0700

    Add files via upload

--- finalproject.py
+++ finalproject.py
@@ -1,43 +1,43 @@
-#make a text based adventure game with between 2 main characters
-#set up sleep for a bit of suspense
-
 import time
-"""this fucntion sets up the story"""
-def intro():
+def main():
+    """this fucntion sets up the story"""
     print("asfsdafsdf")
     print()
-    return intro
+    CharacterChoice()
 
-"""this function contains the different character selection"""
+
 def CharacterChoice():
+    """this function contains the different character selection"""
     choice=''
     print("So now you're presented with a choice.")
     while choice != 'Sam' and choice != 'Alex':#make Sam and Alex the only possible responses or else the next print statement repeats itself
         time.sleep(3)
         choice=str(input("Which character's story would you like to experience first? (Sam or Alex)"))
-    return CharacterChoice
-
-"""this function starts the branching story paths"""
-
-def CharacterPath(choice):
-    choice=input()
     if choice=='Sam':
-        return print("happy")
+        print("happy")
+        SamStart()
     elif choice=='Alex':
-        return print("sad")
+        print("sad")
+        AlexStart()
+    return CharacterChoice()
+
+def SamStart():
+    """this function contains Sam's first choice branch"""
+    print("family")
+    choice2=''
+    while choice2 !='run' and choice2 !='fight':
+        choice2=str(input("run or fight"))
+    if choice2=='run':
+        print('too slow')
+        SamRun()
+    elif choice2=='fight':
+        print('too bad')
+        SamFight()
+    return SamStart()
 
 
 
+def AlexStart():
+    print("fan")
 
-
-
-
-
-
-
-def main():
-    import time
-    intro()
-    CharacterChoice()
-    CharacterPath(choice)
 main()
