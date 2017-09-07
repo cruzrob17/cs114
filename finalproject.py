056788f0b696607031f31a80f7b0d9d621961d96
s203 <s203@AI241LAB0203M12.local>
2017-09-07 16:55:53 -0700

    updates

--- finalproject.py
+++ finalproject.py
@@ -1,22 +1,22 @@
 import time
+import sys
 def main():
     """this fucntion sets up the story"""
     print("asfsdafsdf")
     print()
     CharacterChoice()
 
-
 def CharacterChoice():
     """this function contains the different character selection"""
-    choice=''
+    choice1=''
     print("So now you're presented with a choice.")
-    while choice != 'Sam' and choice != 'Alex':#make Sam and Alex the only possible responses or else the next print statement repeats itself
+    while choice1 != 'Sam' and choice1 != 'Alex':#make Sam and Alex the only possible responses or else the next print statement repeats itself
         time.sleep(3)
-        choice=str(input("Which character's story would you like to experience first? (Sam or Alex)"))
-    if choice=='Sam':
+        choice1=str(input("Which character's story would you like to experience first? (Sam or Alex):"))
+    if choice1=='Sam':
         print("happy")
         SamStart()
-    elif choice=='Alex':
+    elif choice1=='Alex':
         print("sad")
         AlexStart()
     return CharacterChoice()
@@ -25,29 +25,124 @@ def SamStart():
     """this function contains Sam's first choice branch"""
     print("family")
     choice2=''
-    while choice2 !='run' and choice2 !='fight':
-        choice2=str(input("run or fight"))
-    if choice2=='run':
+    while choice2 !='Work' and choice2 !='Appointment':
+        choice2=str(input("Work or Appointment:"))
+    if choice2=='Work':
         print('too slow')
-        SamRun()
-    elif choice2=='fight':
+        SamWork()
+    elif choice2=='Appointment':
         print('too bad')
-        SamFight()
+        SamAppoint()
     return SamStart()
+
+def SamAppoint():
+    """this function contains Sam's choice to go to the appointment"""
+    print("appointment")
+    choice5=''
+    while choice5 != 'Walk' and choice5 != 'Family':
+        choice5=str(input("Walk or Family:"))
+    if choice5=='Walk':
+        print("chill out")
+        GameEnd()
+    elif choice5=='Family':
+        print("help")
+        SamFamily()
+    return SamAppoint()
+
+def SamFamily():
+    """this function contains Sam's choice to visit Alex's family"""
+    print("Family")
+    choice6=''
+    while choice6 != 'Pay' and choice!= 'Home':
+        choice6=str(input("Pay or Home:"))
+    if choice6=='Pay':
+        print("Good person")
+        GameEnd()
+    elif choice6=='Home':
+        print("Kinda selish")
+        GameEnd()
+    return SamFamily()
+
+def SamWork():
+    """this function contains Sam's choice to go to work"""
+    print("work")
+    choice3=''
+    while choice3 !='Home' and choice3 !='End':
+        choice3=str(input("Home or End:"))
+    if choice3=='Home':
+        print("Dad's home")
+        SamHome()
+    elif choice3=='End':
+        print("sucks")
+        GameEnd()
+    return SamWork()
+
+def SamHome():
+    """this function contains Sam's choice to go home"""
+    print("Home")
+    choice4=''
+    while choice4 !='Wife' and choice4 !='BF':
+        choice4=str(input("Wife of BF:"))
+    if choice4=='Wife':
+        print("happy life")
+        GameEnd()
+    elif choice4=='BF':
+        print("sad life")
+        GameEnd()
+    return SamHome()
 
 def AlexStart():
     """this function contains Alex's first choice branch"""
     print("fan")
     choiceB=''
-    while choiceB  !='bat' and choiceB !='fun':
-        choiceB=str(input("bat or fun"))
-    if choiceB=='bat':
+    while choiceB  !='Run' and choiceB !='Talk':
+        choiceB=str(input("Run or Talk:"))
+    if choiceB=='Run':
         print("how high?")
-        AlexBat()
-    elif choiceB=='fun':
+        AlexRun()
+    elif choiceB=='Talk':
         print("not now")
-        AlexFun()
-    return AlexStart()
+        AlexTalk()
+    return AlexTalk()
+
+def AlexRun():
+    """this function contains Alex's choice to run"""
+    print("caught")
+    GameEnd()
+    return AlexRun()
+
+def AlexTalk():
+    """this function contains Alex's choice to talk to the police"""
+    print("Talk")
+    choiceC=''
+    while choiceC !='Fundraiser' and choiceC !='Steal':
+        choiceC=str(input("Fundraiser or Steal:"))
+    if choiceC=='Fundraiser':
+        print("good person")
+        GameEnd()
+    elif choiceC=='Steal':
+        print("sketchy")
+        AlexSteal()
+    return AlexTalk()
+
+def AlexSteal():
+    """this function contains Alex's choice to steal the cure"""
+    print("Steal")
+    choiceD=''
+    while choiceD !='Day' and choiceD !='Night':
+        choiceD=str(input("Day or Night:"))
+    if choiceD=='Day':
+        print("bad choice")
+        GameEnd()
+    elif choiceD=='Night':
+        print("good choice")
+        GameEnd()
+    return AlexSteal()
+
+def GameEnd():
+    """this function ends the game"""
+    print("game over")
+    sys.exit()
 
 
 main()
