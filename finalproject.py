618ddd4a9b55e1b30e9b36c8c61e3c25c75bdfe2
cruzrob17 <rcruz2827@gmail.com>
2017-09-16 19:02:54 -0700

    Add files via upload

--- finalproject.py
+++ finalproject.py
@@ -13,10 +13,10 @@ def main():
     time.sleep(5)
     print("Now enters a broadset man with a hockey mask and a mysterious black bag.")
     print("Attempting to rob the bank, the broadset man grabs the nearest two people next to him and holds them hostage. Those two people just happen to be Sam and Alex.")
-    print("Eventually the cops show up and a stand off begins with them and the boad set man.")
+    print("Eventually the cops show up and a stand off begins with them and the boadset man.")
     print()
     time.sleep(6)
-    print("With tensions growing, the broadset man points a gun at the Sam and Alex while moving shakenly between them.")
+    print("With tensions rising, the broadset man points a gun at Sam and Alex while moving shakenly between them.")
     CharacterChoice()
 
 def CharacterChoice():
@@ -27,10 +27,10 @@ def CharacterChoice():
         time.sleep(3)
         choice1=str(input("Which character would you like to survive and then subsequently experience their story? (Type Sam or Alex):"))
     if choice1=='Sam':
-        print("Boom! The scruff looking teenager drops dead next to you as well as the broadset man behind you.")
+        print("BANG! The scruff looking teenager drops dead next to you as well as the broadset man behind you.")
         SamStart()
     elif choice1=='Alex':
-        print("Boom! The tall slender man drops dead next to you as well as the broadset man behind you.")
+        print("BANG! The tall slender man drops dead next to you as well as the broadset man behind you.")
         AlexStart()
     return CharacterChoice()
 
@@ -40,6 +40,7 @@ def SamStart():
     print()
     print("You see the cops beckon you toward them as you hear a constant ringing in your left ear.")
     print("You're visibly shaken by the whole ordeal but you can't stop thinking about the whole reason why you came to the bank in the first place.")
+    print()
     print("You wake up the next morning to an email from the CPD offering you free counsling for patients with ptsd.")
     time.sleep(6)
     choice2=''
@@ -61,56 +62,89 @@ def SamAppoint():
     print("You didn't need to put your life at risk if you didn't want to abandon your wife")
     print()
     print("You didnt't need to see that kid drop dead next to you and you can't help but feel responsable for everything.")
+    print("The thearapist walks you through the many ways you can go about treating your condition and one of those options include talking to the scruff looking teenager's family.")
+    print("Your not sure if it's a good idea to meet with the family. You don't know if it will only make your feelings worse.")
     choice5=''
     while choice5 != 'Walk' and choice5 != 'Family':
-        choice5=str(input("Walk or Family:"))
+        choice5=str(input("Go on a walk to think things through or bight the bullet and find the family of the dead teenager.(Type Walk or Family):"))
     if choice5=='Walk':
-        print("chill out")
+        print("You choose to take a moment and go for a walk")
+        print("During your walk, you think that maybe infidelity wasn't worth it but then again neither was your life with Lilly, your wife.")
+        print("Why did you have to survive instead of that other kid that undoubtly had so much potential?")
+        print("You find that it'll make sense for you to move far away from Chicago in order to not hurt anybody anymore.")
         GameEnd()
     elif choice5=='Family':
-        print("help")
+        print("You choose to contact the family of the teenager and the therapist gives you their number.")
         SamFamily()
     return SamAppoint()
 
 def SamFamily():
     """this function contains Sam's choice to visit Alex's family"""
-    print("Family")
+    print("You choose to put on a brave face and go meet the family of the killed boy.")
+    print("While with the family, you're overeraught with guilt and and it's unbearable to look at them.")
+    print("Though you may not be fiancialy stable enough to do so, you consider the thought of maybe paying for the familie's funeral expenses.")
+    print("You think that it'll help you be at ease after the traumatic events that had just transpired")
     choice6=''
     while choice6 != 'Pay' and choice!= 'Home':
-        choice6=str(input("Pay or Home:"))
+        choice6=str(input("Now you have to choose whether or not to pay the family's bills(Type Pay or Home):"))
     if choice6=='Pay':
-        print("Good person")
+        print("You choose to offer the family some miney in order for them to pay the funeral cost.")
+        print("However they refuse yet ask that you attend the service the following week and you oblige.")
+        print("")
         GameEnd()
     elif choice6=='Home':
-        print("Kinda selish")
+        print("You choose to go home and forgo the funeral because you believe that it'll be too hard on you to go")
+        print("You hope to muddle things over with yourslef and drown your worries in drink. A pretty bad idea but it'll do for now.")
         GameEnd()
     return SamFamily()
 
 def SamWork():
     """this function contains Sam's choice to go to work"""
-    print("work")
+    print("You brush off your conflicted emotions in order to impress this major client at work.")
+    print()
+    print("As you await your important meeting, your leg can't stop shaking and you find yourself out of breath due to your overacctive nervs.")
+    print("The meeeting starts and your big payday lies in the hands of a burly middle aged man with a brown mustache.")
+    time.sleep(6)
+    print("While giving the mustached man a well rehearsed pitch on a fantastic new cardboard line that your company can provide, your brain goes on autopilot.")
+    print("Just like you never left the bank, you start to hear yelling and the sound peircing bang of a gunshot.")
+    print("Your speech patterns start to blur together and you find yourself on the ground.")
+    print("Struggling to get to your feet, you find no better option than to escape your embarrassment by crawling to the door.")
+    print("Once outside, you can hear the confused murmers of your co-workers and client and give yourslef two options.")
     choice3=''
-    while choice3 !='Home' and choice3 !='End':
-        choice3=str(input("Home or End:"))
+    while choice3 !='Home' and choice3 !='Roof':
+        choice3=str(input("Go home and pretty much quit your job or go up to the roof and gather your thoughts (Type Home or Roof):"))
     if choice3=='Home':
-        print("Dad's home")
+        print("You quickly gather your things and begin heading home.")
         SamHome()
-    elif choice3=='End':
-        print("sucks")
+    elif choice3=='Roof':
+        print("You make a mad dash to the roof through way of the many flights of stairs.")
+        print("The door to the roof is slamed open by you and you're greeted by the twinkling of the cityscape.")
+        print("You wipe the sweat from your brow and move to the waist high edge of the bulding. ")
+        print("Your heart tells you to jump but your brain tells you to call Lilly, your wife.")
+        print("You see the disapointed faces of those you love and the scruff looking teenager flash in your eyes.")
+        print("Like most things in your life, you fail to listen to your brain and your heart got the better of you")
         GameEnd()
     return SamWork()
 
 def SamHome():
     """this function contains Sam's choice to go home"""
-    print("Home")
+    print("You choose to go home to be with your family in order to get you through your breakdown.")
+    print("You're unfortunately greeted by an enraged Lilly, your wife.")
+    print("At the same time though, you could't be happier to see her.")
+    print("So you sit her down and explain to her everything that's happend these past couple days because you haven't yet.")
+    print("While explaining your story to her, see your kids running from their rooms to embrace you.")
+    print("You feel you're at a crossroads in your lives and need to make a decisions.")
     choice4=''
-    while choice4 !='Wife' and choice4 !='BF':
-        choice4=str(input("Wife of BF:"))
+    while choice4 !='Wife' and choice4 !='GF':
+        choice4=str(input("Choose to stay with your wife in order to stay in contact with your kids or choose your girlfriend in order to be happy(Type Wife or GF):"))
     if choice4=='Wife':
-        print("happy life")
+        print("You choose to work things out with your wife for the sake of your children.")
+        print("You still have nightmares of taht day but things start to feel normal again.")
+        print("You even begin to think that maybe things will start to be ok.")
         GameEnd()
-    elif choice4=='BF':
-        print("sad life")
+    elif choice4=='GF':
+        print("You choose to make the hard decision of moving out to be with your girlfriend.")
+        print("It's not an easy transition but deep down you think that in the long run, you'll be happier.")
         GameEnd()
     return SamHome()
 
